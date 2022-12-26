33f8902af411223cb27104c55d955b3ae5187498
Romaric Philogène <rphilogene@qovery.com>
2022-12-26 09:11:07 +0100

    fix: aws-lambda-with-serverless example

--- examples/aws-lambda-with-serverless/remove.sh
+++ examples/aws-lambda-with-serverless/remove.sh
@@ -1,9 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
 # substitute QOVERY_ENVIRONMENT_ID by a shorter version - QOVERY_ENVIRONMENT_ID is too long for a lambda name (UUID v4)
 export QOVERY_ENVIRONMENT_ID=$(echo "$QOVERY_ENVIRONMENT_ID" | cut -d "-" -f 1)
 
-# Execute the given or default command:
-/bin/bash -l -c "$@"
+serverless remove --stage $QOVERY_ENVIRONMENT_ID
