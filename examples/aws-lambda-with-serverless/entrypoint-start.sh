04c98477109434951fbe3610930f94b45a95cc0c
Romaric Philogène <rphilogene@qovery.com>
2022-12-26 01:50:05 +0100

    fix: aws-lambda-with-serverless example

---
+++ examples/aws-lambda-with-serverless/entrypoint-start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+# substitute QOVERY_ENVIRONMENT_ID by a shorter version - QOVERY_ENVIRONMENT_ID is too long for a lambda name (UUID v4)
+export QOVERY_ENVIRONMENT_ID=$(echo "$QOVERY_ENVIRONMENT_ID" | cut -d "-" -f 1)
+
+serverless deploy --stage $QOVERY_ENVIRONMENT_ID
+serverless manifest --stage $QOVERY_ENVIRONMENT_ID -p qovery-output.js
