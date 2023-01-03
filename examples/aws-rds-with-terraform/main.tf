78ae72788dead75bb74d576975e33c807f8be697
Romaric Philogène <rphilogene@qovery.com>
2023-01-03 11:12:08 -0300

    Update main.tf

--- examples/aws-rds-with-terraform/main.tf
+++ examples/aws-rds-with-terraform/main.tf
@@ -20,7 +20,6 @@ provider "aws" {
 
 resource "aws_db_instance" "rds_instance" {
   allocated_storage   = 20
-  identifier          = "rds-terraform"
   storage_type        = "gp2"
   engine              = "mysql"
   engine_version      = "8.0.27"
