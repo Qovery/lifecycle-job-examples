00ba899eb2dc5f3825b99531fcde87756a80e71a
Romaric P <rphilogene@qovery.com>
2024-10-27 10:22:40 +0100

    chore: update AWS RDS with terraform

--- examples/aws-rds-with-terraform/main.tf
+++ examples/aws-rds-with-terraform/main.tf
@@ -2,11 +2,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.27"
+      version = "~> 5.0"
     }
   }
-
-  backend "s3" {}
 
   required_version = ">= 0.14.9"
 }
@@ -17,18 +15,23 @@ provider "aws" {
   secret_key = var.aws_secret_access_key
 }
 
+resource "random_password" "password" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
 
 resource "aws_db_instance" "rds_instance" {
-  allocated_storage   = 20
-  storage_type        = "gp2"
-  engine              = "mysql"
-  engine_version      = "8.0.27"
-  instance_class      = "db.t2.micro"
-  name                = "myDbName${split("-", var.qovery_environment_id)[0]}"
-  username            = "yourDbUsername"
-  password            = "yourPassword"
-  publicly_accessible = true
-  skip_final_snapshot = true
+  db_instance_identifier = "myDbName${split("-", var.qovery_environment_id)[0]}"
+  allocated_storage      = 20
+  storage_type           = "gp2"
+  engine                 = "mysql"
+  engine_version         = "8.0.39"
+  instance_class         = "db.t2.micro"
+  username               = var.username
+  password               = length(trimspace(var.password)) > 0 ? var.password : random_password.password.result
+  publicly_accessible    = true
+  skip_final_snapshot    = true
 
   tags = {
     ttl = 15552000
